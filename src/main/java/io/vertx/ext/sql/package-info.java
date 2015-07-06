cb9db1b9e4ffdc53b55a3b16dae28af24fa5108f
Clement Escoffier <clement.escoffier@gmail.com>
2015-07-06 15:44:29 +0200

    Add dependency information to the documentation
    Related to: https://github.com/vert-x3/issues/issues/14

--- src/main/java/io/vertx/ext/sql/package-info.java
+++ src/main/java/io/vertx/ext/sql/package-info.java
@@ -3,8 +3,28 @@
  *
  * The common SQL interface is used to interact with Vert.x SQL services.
  *
- * You obtain a connection to the database via the service interface for the specific SQL service that you are using
- * (e.g. JDBC/MySQL/PostgreSQL).
+ * You obtain a connection to the database via the service interface for the specific SQL service that
+ * you are using (e.g. JDBC/MySQL/PostgreSQL).
+ *
+ * To use this project, add the following dependency to the _dependencies_ section of your build descriptor:
+ *
+ * * Maven (in your `pom.xml`):
+ *
+ * [source,xml,subs="+attributes"]
+ * ----
+ * <dependency>
+ *   <groupId>{maven-groupId}</groupId>
+ *   <artifactId>{maven-artifactId}</artifactId>
+ *   <version>{maven-version}</version>
+ * </dependency>
+ * ----
+ *
+ * * Gradle (in your `build.gradle` file):
+ *
+ * [source,groovy,subs="+attributes"]
+ * ----
+ * compile {maven-groupId}:{maven-artifactId}:{maven-version}
+ * ----
  *
  * == The SQL Connection
  *
