472293c1cebf661c395d85911f89c8bd4738ca0a
Clement Escoffier <clement.escoffier@gmail.com>
2015-10-02 18:20:34 +0200

    Replace asciidoc attributes by docgen variables

--- src/main/java/io/vertx/ext/sql/package-info.java
+++ src/main/java/io/vertx/ext/sql/package-info.java
@@ -13,9 +13,9 @@
  * [source,xml,subs="+attributes"]
  * ----
  * <dependency>
- *   <groupId>{maven-groupId}</groupId>
- *   <artifactId>{maven-artifactId}</artifactId>
- *   <version>{maven-version}</version>
+ *   <groupId>${maven.groupId}</groupId>
+ *   <artifactId>${maven.artifactId}</artifactId>
+ *   <version>${maven.version}</version>
  * </dependency>
  * ----
  *
@@ -23,7 +23,7 @@
  *
  * [source,groovy,subs="+attributes"]
  * ----
- * compile {maven-groupId}:{maven-artifactId}:{maven-version}
+ * compile ${maven.groupId}:${maven.artifactId}:${maven.version}
  * ----
  *
  * == The SQL Connection
