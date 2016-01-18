f3391955bebc1207800e40cd59d1c5490be72268
Clement Escoffier <clement.escoffier@gmail.com>
2016-01-18 08:47:44 +0100

    fix gradle dependency

--- src/main/java/io/vertx/ext/sql/package-info.java
+++ src/main/java/io/vertx/ext/sql/package-info.java
@@ -23,7 +23,7 @@
  *
  * [source,groovy,subs="+attributes"]
  * ----
- * compile ${maven.groupId}:${maven.artifactId}:${maven.version}
+ * compile '${maven.groupId}:${maven.artifactId}:${maven.version}'
  * ----
  *
  * == The SQL Connection
