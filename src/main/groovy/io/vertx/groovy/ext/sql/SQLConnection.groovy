4fb5e51dd8e66b2ecd7a156c853f0c641622a0c5
Paulo Lopes <paulo@mlopes.net>
2016-05-03 09:08:13 +0200

    Added documentation for batching

--- src/main/groovy/io/vertx/groovy/ext/sql/SQLConnection.groovy
+++ src/main/groovy/io/vertx/groovy/ext/sql/SQLConnection.groovy
@@ -19,6 +19,7 @@ import groovy.transform.CompileStatic
 import io.vertx.lang.groovy.InternalHelper
 import io.vertx.core.json.JsonObject
 import io.vertx.core.json.JsonArray
+import java.util.List
 import io.vertx.ext.sql.UpdateResult
 import io.vertx.ext.sql.ResultSet
 import io.vertx.core.AsyncResult
@@ -206,6 +207,66 @@ public class SQLConnection {
    */
   public SQLConnection rollback(Handler<AsyncResult<Void>> handler) {
     delegate.rollback(handler);
+    return this;
+  }
+  /**
+   * Batch simple SQL strings and execute the batch where the async result contains a array of Integers.
+   * @param sqlStatements sql statement
+   * @param handler the result handler
+   * @return 
+   */
+  public SQLConnection batch(List<String> sqlStatements, Handler<AsyncResult<List<Integer>>> handler) {
+    delegate.batch(sqlStatements != null ? (List)sqlStatements.collect({it}) : null, handler != null ? new Handler<AsyncResult<java.util.List<java.lang.Integer>>>() {
+      public void handle(AsyncResult<java.util.List<java.lang.Integer>> ar) {
+        if (ar.succeeded()) {
+          handler.handle(io.vertx.core.Future.succeededFuture(ar.result()));
+        } else {
+          handler.handle(io.vertx.core.Future.failedFuture(ar.cause()));
+        }
+      }
+    } : null);
+    return this;
+  }
+  /**
+   * Batch a prepared statement with all entries from the args list. Each entry is a batch.
+   * The operation completes with the execution of the batch where the async result contains a array of Integers.
+   * @param sqlStatement sql statement
+   * @param args the prepared statement arguments
+   * @param handler the result handler
+   * @return 
+   */
+  public SQLConnection batchWithParams(String sqlStatement, List<List<Object>> args, Handler<AsyncResult<List<Integer>>> handler) {
+    delegate.batchWithParams(sqlStatement, args != null ? (List)args.collect({new io.vertx.core.json.JsonArray(it)}) : null, handler != null ? new Handler<AsyncResult<java.util.List<java.lang.Integer>>>() {
+      public void handle(AsyncResult<java.util.List<java.lang.Integer>> ar) {
+        if (ar.succeeded()) {
+          handler.handle(io.vertx.core.Future.succeededFuture(ar.result()));
+        } else {
+          handler.handle(io.vertx.core.Future.failedFuture(ar.cause()));
+        }
+      }
+    } : null);
+    return this;
+  }
+  /**
+   * Batch a callable statement with all entries from the args list. Each entry is a batch.
+   * The size of the lists inArgs and outArgs MUST be the equal.
+   * The operation completes with the execution of the batch where the async result contains a array of Integers.
+   * @param sqlStatement sql statement
+   * @param inArgs the callable statement input arguments
+   * @param outArgs the callable statement output arguments
+   * @param handler the result handler
+   * @return 
+   */
+  public SQLConnection batchCallableWithParams(String sqlStatement, List<List<Object>> inArgs, List<List<Object>> outArgs, Handler<AsyncResult<List<Integer>>> handler) {
+    delegate.batchCallableWithParams(sqlStatement, inArgs != null ? (List)inArgs.collect({new io.vertx.core.json.JsonArray(it)}) : null, outArgs != null ? (List)outArgs.collect({new io.vertx.core.json.JsonArray(it)}) : null, handler != null ? new Handler<AsyncResult<java.util.List<java.lang.Integer>>>() {
+      public void handle(AsyncResult<java.util.List<java.lang.Integer>> ar) {
+        if (ar.succeeded()) {
+          handler.handle(io.vertx.core.Future.succeededFuture(ar.result()));
+        } else {
+          handler.handle(io.vertx.core.Future.failedFuture(ar.cause()));
+        }
+      }
+    } : null);
     return this;
   }
 }
