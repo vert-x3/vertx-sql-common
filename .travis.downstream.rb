5806234a8f44eda63e6b56ce622daabd86b62fd4
Thomas Segismont <tsegismo@redhat.com>
2016-11-28 12:52:30 +0100

    Travis: fix dependent build trigger script
    We should restart dependent builds only when
     - the branch is master
     - not building a PR
     - building a SNASPHOT version

--- .travis.downstream.rb
+++ .travis.downstream.rb
@@ -3,9 +3,15 @@ require 'travis'
 
 Travis.access_token = ENV['TRAVIS_API_TOKEN']
 
-config = YAML.load_file('.travis.yml')
+is_snapshot = ENV['PROJECT_VERSION'] =~ /.*SNAPSHOT/
+is_master = ENV['TRAVIS_BRANCH'] == 'master'
+not_pull_request = ENV['TRAVIS_PULL_REQUEST'] == 'false'
 
-config["downstream"].each { |downstream|
-  repository = Travis::Repository.find(downstream)
-  repository.branch('master').restart
-}
+if is_snapshot and is_master and not_pull_request
+  config = YAML.load_file('.travis.yml')
+
+  config["downstream"].each { |downstream|
+    repository = Travis::Repository.find(downstream)
+    repository.branch('master').restart
+  }
+end
