4394edd27163e6475ae0f0f6e940014ddec51f21
Paulo Lopes <paulo@mlopes.net>
2017-04-11 10:04:27 +0200

    Copyright headers

--- src/main/java/io/vertx/ext/sql/SQLOptions.java
+++ src/main/java/io/vertx/ext/sql/SQLOptions.java
@@ -1,8 +1,28 @@
+/*
+ * Copyright (c) 2011-2014 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
 package io.vertx.ext.sql;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.json.JsonObject;
 
+/**
+ * Represents the options one can use to customize the unwrapped connection/statement/resultset types
+ *
+ * @author <a href="mailto:plopes@redhat.com">Paulo Lopes</a>
+ */
 @DataObject(generateConverter = true)
 public class SQLOptions {
 
