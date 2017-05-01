c7f464fb74605e80e026d2afb8da45ff8f466ab0
Emad Alblueshi <emad.albloushi@gmail.com>
2017-05-01 14:49:22 +0300

    Add common asynchronous client interface

---
+++ src/main/java/io/vertx/ext/sql/SQLClient.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011-2014 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package io.vertx.ext.sql;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.VertxGen;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+
+/**
+ * A common asynchronous client interface for interacting with SQL compliant database
+ *
+ * @author <a href="mailto:emad.albloushi@gmail.com">Emad Alblueshi</a>
+ */
+@VertxGen
+public interface SQLClient {
+
+  /**
+   * Returns a connection that can be used to perform SQL operations on. It's important to remember
+   * to close the connection when you are done, so it is returned to the pool.
+   *
+   * @param handler the handler which is called when the <code>JdbcConnection</code> object is ready for use.
+   */
+  @Fluent
+  SQLClient getConnection(Handler<AsyncResult<SQLConnection>> handler);
+
+  /**
+   * Close the client
+   */
+  void close();
+}
